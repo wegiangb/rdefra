e0a4388e52a9335c1a442a0ff810fddecf0e5e78
Claudia Vitolo <claudia.vitolo@gmail.com>
2018-07-08 15:11:47 +0100

    Remove requirement for linter free code

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,3 @@ library("testthat")
 library("rdefra")
 
 test_check("rdefra")
-
-# Static code analysis using the lintr package
-# Integration with lintr: tests to fail if there are any lints in the project
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("Package Style", {
-    lintr::expect_lint_free()
-  })
-}
